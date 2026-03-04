f5ed028bd65af69011e74fb196d8d56f26a9d16a
berkaybildaci <berkaybildaci@gmail.com>
2026-03-04 12:02:48 -0600

    init commit

---
+++ Assets/Scripts/ScriptingTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ScriptingTest : MonoBehaviour
+{
+    [SerializeField] float testVar;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
